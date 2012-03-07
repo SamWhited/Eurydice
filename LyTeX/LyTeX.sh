b9971bd0107728910e93beb3d036a6831f27b69b
Sam Whited <sam@samwhited.com>
2012-03-06 23:54:47 -0500

    Updated LyTeX to be more robust.

--- LyTeX/LyTeX.sh
+++ LyTeX/LyTeX.sh
@@ -2,18 +2,33 @@
 
 set -e
 
-BASENAME=$1
-SUFFIX=$2
+FILENAME=$1
+DIRNAME=${FILENAME%/*}
+if [ "$DIRNAME" == "$FILENAME" ]; then
+	DIRNAME="./"
+fi
+BASENAME=$(basename $FILENAME)
+BASENAME=${BASENAME%.*}
+SUFFIX=${FILENAME##*.}
 
-echo Basename = $BASENAME
-echo Suffix = $SUFFIX
+echo "Directory = $DIRNAME"
+echo "Basename = $BASENAME"
+echo "Suffix = $SUFFIX"
 if [ -a $BASENAME ]; then
 	rm -rf $BASENAME
 fi
 
-lilypond-book --output=$BASENAME --pdf $BASENAME.$SUFFIX
+lilypond-book --output=$BASENAME --pdf $FILENAME
 
-cd $BASENAME
-pdflatex --synctex=1 $BASENAME.tex
+(cd $BASENAME; pdflatex --synctex=1 $BASENAME.tex)
 
-cp $BASENAME.pdf ../
+cp $BASENAME/$BASENAME.pdf ./$BASENAME.pdf >/dev/null 2>&1 || { echo >&2 "Error copying PDF!"; }
+
+echo "Copying midi files..."
+MIDIDIR=$(find `ls -l | awk ' /^d/ { print $NF } '` -type d | grep $BASENAME | tail -n 1)
+cp $MIDIDIR/*.midi ./$BASENAME.midi >/dev/null 2>&1 || { echo >&2 "No midi output detected."; }
+
+command -v timidity >/dev/null 2>&1 || { echo >&2 "Timidity is not installed. No flac file will be generated."; exit 0; }
+
+echo "Generating flac files..."
+timidity -OF $BASENAME.midi
